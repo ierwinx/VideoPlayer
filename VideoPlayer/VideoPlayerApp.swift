ce25f7e49ccce75faa7701f0029d1439946e5d6b
Eяw!и Gontier <ierwinx@me.com>
2022-10-28 10:53:24 -0500

    Initial Commit

---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by Erwin Luz León on 28/10/22.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
